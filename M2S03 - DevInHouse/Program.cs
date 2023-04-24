5174be9c5af1ae0894f462721eaa2e1cf0746bda
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-04-24 17:39:40 -0300

    [M2S03] Ex 1 - Criar Método Soma de números

--- M2S03 - DevInHouse/Program.cs
+++ M2S03 - DevInHouse/Program.cs
@@ -1,3 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
 
+
+         int Soma(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+        int resultado = Soma(3, 5);
+
+        Console.WriteLine(resultado);
+
+
+    }
+}
