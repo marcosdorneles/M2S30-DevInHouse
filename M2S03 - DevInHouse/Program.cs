65c3a80647f96a25baed373d89bf90612be65e31
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-04-24 18:19:26 -0300

    [M2S03] Ex 2 - Criar Sobrecargas do método Soma de números

--- M2S03 - DevInHouse/Program.cs
+++ M2S03 - DevInHouse/Program.cs
@@ -5,16 +5,32 @@
         Console.WriteLine("Hello, World!");
 
 
-         int Soma(int num1, int num2)
+        int Soma(int num1, int num2)
         {
             int resultado = num1 + num2;
             return resultado;
         }
 
-        int resultado = Soma(3, 5);
+        double Soma(double num1, double num2)
+        {
+            double resultado = num1 + num2;
+            return resultado;
+        }
 
-        Console.WriteLine(resultado);
+         double Soma(double num1, double num2, double num3)
+        {
+            double resultado = num1 + num2 + num3;
+            return resultado;
+        }
 
+
+        int resultado1 = Soma(3, 5); 
+        double resultado2 = Soma(2.5, 3.7); 
+        double resultado3 = Soma(1.0, 2.0, 3.0);
+
+        Console.WriteLine(resultado1);
+        Console.WriteLine(resultado2);
+        Console.WriteLine(resultado3);
 
     }
 }
