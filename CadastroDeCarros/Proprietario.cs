c30b1592de5ff2a74826700eb2c260a5041db0d1
Marcos Guilherme Dorneles Staub <marcosguilhermedornelesstaub@MacBook-Air-de-Marcos.local>
2023-04-24 19:11:24 -0300

    [M2S03] Ex 4 - Crie Instâncias para a classe Proprietario

--- CadastroDeCarros/Proprietario.cs
+++ CadastroDeCarros/Proprietario.cs
@@ -3,9 +3,16 @@ namespace CadastroDeCarros
 {
 	public class Proprietario
 	{
-		public Proprietario()
-		{
-		}
-	}
+        public Proprietario()
+		{}
+
+
+        private String nome { get; set; }
+        private String cpf { get; set; }
+        private String telefone { get; set; }
+
+        Proprietario primeiroProprietario = new Proprietario();
+        
+    }
 }
 
